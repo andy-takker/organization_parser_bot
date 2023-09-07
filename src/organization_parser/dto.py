e02b07351e2104d81a6fcae92502749dbde1372a
Sergey Natalenko <sergey.natalenko@mail.ru>
2023-09-07 13:21:43 +0300

    Add typer cli

--- src/organization_parser/dto.py
+++ src/organization_parser/dto.py
@@ -6,10 +6,27 @@ class Company:
     name: str
     address: str
     url: str | None = None
+    email: str | None = None
+    phone: str | None = None
 
     def __eq__(self, other):
         return (
-            self.name == other.name
+            self.__class__.__name__ == other.__class__.__name__
+            and self.name == other.name
             and self.address == other.address
             and self.url == other.url
+            and self.email == other.email
+            and self.phone == other.phone
+        )
+
+    def __repr__(self) -> str:
+        return f"{self.name} {self.address} {self.url} {self.email} {self.phone}"
+
+    def to_dict(self) -> dict[str, str | None]:
+        return dict(
+            name=self.name,
+            address=self.address,
+            url=self.url,
+            email=self.email,
+            phone=self.phone,
         )
--- src/organization_parser/utils.py
+++ src/organization_parser/utils.py
@@ -1,25 +1,57 @@
+import csv
+import logging
 import math
+from pathlib import Path
 from typing import Any, Literal
 
 import requests
 
 from .dto import Company
 
+logger = logging.getLogger(__name__)
+
 QueryType = Literal["biz", "geo"]
 
 REQUEST_API_URL = "https://search-maps.yandex.ru/v1/"
 
 
+def save_companies_to_csv_file(companies: list[Company], output_filename: Path) -> None:
+    with open(output_filename, "w+") as f:
+        writer = csv.DictWriter(
+            f, fieldnames=["name", "address", "email", "phone", "url"]
+        )
+        writer.writeheader()
+        writer.writerows(map(lambda c: c.to_dict(), companies))
+    logger.info("Companies was written into %s", output_filename)
+
+
+def get_companies_dump(
+    api_key: str, location: str, company_query: str, radius_km: float
+) -> list[Company]:
+    toponym_location = get_toponym_location(api_key=api_key, query=location)
+    return sorted(
+        get_all_results(
+            api_key=api_key,
+            query=company_query,
+            ll=toponym_location,  # type: ignore[arg-type]
+            radius_km=radius_km,
+        ),
+        key=lambda x: x.name,
+    )
+
+
 def get_all_results(
-    apikey: str, query: str, ll: tuple[float, float], radius: float
+    api_key: str, query: str, ll: tuple[float, float], radius_km: float
 ) -> set[Company]:
-    r = km_to_ll(radius)
-    spn = (r, r)
+    radius_radian = km_to_ll(radius_km)
+    spn = (radius_radian, radius_radian)
     skip = 0
+    k = 1
     total_companies = set()
     while True:
+        logger.info("Make %d request...", k)
         result = search_on_maps(
-            apikey=apikey,
+            api_key=api_key,
             query=query,
             type_="biz",
             ll=ll,
@@ -27,26 +59,32 @@ def get_all_results(
             skip=skip,
         )
         if not result.ok:
+            logger.error(
+                "Occurred error. Status code: %d. message: `%s`",
+                result.status_code,
+                result.json()["message"],
+            )
             break
         data = result.json()
         total_companies.update(parse_companies(data))
         total = data["properties"]["ResponseMetaData"]["SearchResponse"]["found"]
-        print(total)
         skip += len(data["features"])
         if skip >= total:
             break
+        k += 1
     return total_companies
 
 
-def get_toponym_location(apikey: str, query: str) -> tuple[float, ...]:
-    result = search_on_maps(apikey=apikey, query=query, type_="geo")
+def get_toponym_location(api_key: str, query: str) -> tuple[float, ...]:
+    logger.info("Search location coordinates")
+    result = search_on_maps(api_key=api_key, query=query, type_="geo")
     if not result.ok:
         raise ValueError(f"Result is not OK. Get status code - {result.status_code}")
     return tuple(map(float, result.json()["features"][0]["geometry"]["coordinates"]))
 
 
 def search_on_maps(
-    apikey: str,
+    api_key: str,
     query: str,
     type_: QueryType,
     ll: tuple[float, float] | None = None,
@@ -59,7 +97,7 @@ def search_on_maps(
         "lang": lang,
         "type": type_,
         "results": results,
-        "apikey": apikey,
+        "apikey": api_key,
         "text": query,
         "skip": skip,
     }
